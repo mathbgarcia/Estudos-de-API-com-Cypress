c81b3f0abb723d1746d5d65c56e78fe5f7e88d95
mathbgarcia <99487686+mathbgarcia@users.noreply.github.com>
2022-02-22 11:52:24 -0300

    Create Teste_API.js

---
+++ Teste_API.js
@@ -0,0 +1,23 @@
+@@ -0,0 +1,22 @@
+<sefertos="cipreste" />
+// ***********************************************************
+Este exemplo plugins/índice.js pode ser usado para carregar plugins
+//
+Você pode alterar a localização deste arquivo ou desativar o carregamento
+o arquivo plugins com a opção de configuração 'pluginsFile'.
+//
+Você pode ler mais aqui:
+https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+Essa função é chamada quando um projeto é aberto ou reaberto (por exemplo, devido a
+a mudança de config do projeto)
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+eslint-desativar-próxima-linha não-não-used-vars
+módulo. exportações = (on, config) => {
+  'on' é usado para enganchar em vários eventos Cypress emite
+  'config' é o config Cypress resolvido
+}
